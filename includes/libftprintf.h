b054e72903147d4b368f00401b2255039e7beecb
Samuro Norcross <snorcros@ox-g1.21-school.ru>
2019-11-03 15:53:32 +0300

    add print and fun for c

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -16,7 +16,7 @@
 typedef struct s_funtype
 {
 	char	flag;
-	char	*(*fun)(void *);
+	char	*(*fun)(va_list);
 }				t_funtype;
 
 typedef struct s_placeholder
@@ -29,14 +29,7 @@ typedef struct s_placeholder
     t_funtype	type;
 }               t_placeholder;
 
-t_funtype	types[] =
-{
-	{'c', ft_putchar},
-	{'s', ft_putstr},
-	{'m', NULL}
-};
-
-int				ft_printf(const char * restrict format, ...);
+int				ft_printf(const char *format, ...);
 
 t_placeholder	new_placeholder();
 int 			set_type(t_placeholder *pPlaceholder, char *format);
@@ -44,3 +37,16 @@ int 			set_flags(t_placeholder *pPlaceholder, char *format);
 int 			set_width(t_placeholder *pPlaceholder, char *format, va_list ap);
 int 			set_precision(t_placeholder *pPlaceholder, char *format);
 int 			set_length(t_placeholder *pPlaceholder, char *format);
+
+//char			*type_to_char(t_funtype ftype, va_list ap);
+
+char			*to_str_logic(t_placeholder place, va_list ap);
+char			*ft_c(va_list ap);
+int				print_this(char *str);
+
+t_funtype	types[] =
+{
+		{'c', ft_c},
+		//{'s', ft_putstr},
+		{'m', NULL}
+};
