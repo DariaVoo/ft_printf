da2a34e348b980b99275d7880b3a8300c7ab5a8f
Samuro Norcross <snorcros@oa-e5.21-school.ru>
2020-02-04 21:47:00 +0300

    norme precision.c

--- srcs/precision.c
+++ srcs/precision.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   precision.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snorcros <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/04 21:33:52 by snorcros          #+#    #+#             */
+/*   Updated: 2020/02/04 21:46:33 by snorcros         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libftprintf.h"
 
@@ -30,76 +41,82 @@ static size_t	str_len_mantiss(char *str)
 	return (count);
 }
 
-char *get_precision_float(t_placeholder *place, char *ans)
+char			*get_precision_float(t_placeholder *place, char *ans)
 {
-	size_t count_digit;
+	size_t	count_digit;
 	size_t	i;
+	size_t	j;
 
 	count_digit = strcount_digit(ans);
+	j = place->precision + count_digit;
 	if (place->precision == 0)
 		return (ft_strcut(ans, count_digit - 1));
 	else if ((size_t)place->precision > str_len_mantiss(ans))
-		return (ft_stradd_back(ans, place->precision + count_digit, '0'));
-	if (ans[place->precision + count_digit] >= '5')
+		return (ft_stradd_back(ans, j, '0'));
+	if (ans[j] >= '5')
 	{
 		i = 1;
-		if (ans[place->precision + count_digit - i] != '9')
-			ans[place->precision + count_digit - i] += 1;
-		else {
-			while (ans[place->precision + count_digit - i] == '9') {
-				ans[place->precision + count_digit - i] = '0';
+		j -= i;
+		if (ans[j] != '9')
+			ans[j] += 1;
+		else
+		{
+			while (ans[j] == '9')
+			{
+				ans[j] = '0';
 				i++;
+				j -= i;
 			}
-			ans[place->precision + count_digit - i] += 1;
+			ans[j] += 1;
 		}
 	}
-	return (ft_strcut(ans, place->precision + count_digit));
+	return (ft_strcut(ans, j));
 }
 
+static char		*get_precision_p(t_placeholder *place, char *ans)
+{
+	if (ans[2] == '0')
+	{
+		if (place->precision == 0)
+			ans = ft_strcut(ans, 2);
+		else
+			ans = ft_stradd_back(ans, place->precision + 2, '0');
+	}
+	else
+	{
+		if ((size_t)place->precision > ft_strlen(ans) - 2)
+		{
+			ans[1] = '0';
+			ans = ft_stradd_front(ans, place->precision + 2, '0');
+			ans[1] = 'x';
+		}
+	}
+	return (ans);
+}
 
-
-char *get_precision(t_placeholder *place, char *ans)
+char			*get_precision(t_placeholder *place, char *ans)
 {
 	if (place->type.flag == 'c' || (place->type.flag == 'o' &&
-									(place->flags & FLG_HASH) != 0 && *ans == '0'))
+							(place->flags & FLG_HASH) != 0 && *ans == '0'))
 		return (ans);
 	if (place->type.flag == 'f')
 		return (get_precision_float(place, ans));
 	if (place->type.flag == 'p')
-	{
-		if (ans[2] == '0')
-		{
-			if (place->precision == 0)
-				ans = ft_strcut(ans, 2);
-			else
-				ans = ft_stradd_back(ans, place->precision + 2, '0');
-		}
-		else
-		{
-			if ((size_t)place->precision > ft_strlen(ans) - 2)
-			{
-				ans[1] = '0';
-				ans = ft_stradd_front(ans, place->precision + 2, '0');
-				ans[1] = 'x';
-			}
-		}
-	}
+		return (get_precision_p(place, ans));
 	else if (place->precision == 0 && *ans == '0')
 	{
 		free(ans);
-		place->flags &= ~FLG_HASH;//вроде как убирается только хеш
+		place->flags &= ~FLG_HASH;
 		if (place->type.flag == '%')
 			place->flags &= FLG_NULL;
 		return (ft_strnew(1));
 	}
-	else if ((place->type.flag == 'd' || place->type.flag == 'i' || place->type.flag == 'o'
-			  || place->type.flag == 'u' || place->type.flag == 'x' || place->type.flag == 'X')
-			 && (place->precision > 0 && *ans != '\0'))
-	{
-		//place->flags &= ~FLG_ZERO;
-		if ((size_t)place->precision > ft_strlen(ans))
-			ans = ft_stradd_front(ans, place->precision, '0');
-	}
+	else if ((place->type.flag == 'd' || place->type.flag == 'i'
+	|| place->type.flag == 'o' || place->type.flag == 'u'
+	|| place->type.flag == 'x' || place->type.flag == 'X')
+	&& (place->precision > 0 && *ans != '\0')
+	&& ((size_t)place->precision > ft_strlen(ans)))
+		ans = ft_stradd_front(ans, place->precision, '0');
 	else if (place->type.flag == 's' && place->precision >= 0)
 		ans = ft_strcut(ans, place->precision);
 	return (ans);
